f02b7f82f6dd725fe011f56c02b88965c0aad14e
Duane van Twest <dvtrain@outlook.com>
2018-01-17 09:33:51 +1000

    fix frontend index view for variable categories

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -21,8 +21,8 @@ class FrontendController extends Controller
         ->with('first_post', Post::orderBy('created_at', 'desc' )->first())
         ->with('second_post', Post::orderBy('created_at', 'desc' )->skip(1)->take(1)->get()->first())
         ->with('third_post', Post::orderBy('created_at', 'desc' )->skip(2)->take(1)->get()->first())
-        ->with('laravel', Category::find(1))
-        ->with('vuejs', Category::find(5))
+        // ->with('laravel', Category::find(1))
+        // ->with('vuejs', Category::find(5))
         ->with('settings', Setting::first());
     }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -205,110 +205,44 @@
         </div>
     </div>
 
-
     <div class="container-fluid">
         <div class="row medium-padding120 bg-border-color">
             <div class="container">
               <div class="col-lg-12">
-                <div class="offers">
-                    <div class="row">
-                        <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-                            <div class="heading">
-                                <h4 class="h1 heading-title">{{ $laravel->name }}</h4>
-                                <div class="heading-line">
-                                    <span class="short-line"></span>
-                                    <span class="long-line"></span>
-                                </div>
-                            </div>
-                        </div>
+                @foreach($categories as $c)
+                  @if( $c->posts()->count() > 0 )
+                    <div>
+                      <div class="padded-50"></div>
+                      <div class="offers">
+                          <div class="row">
+                              <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+                                  <div class="heading">
+                                      <h4 class="h1 heading-title">{{ $c->name }}</h4>
+                                      <div class="heading-line">
+                                          <span class="short-line"></span>
+                                          <span class="long-line"></span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="row">
+                              <div class="case-item-wrap">
+                                @foreach($c->posts()->orderBy('created_at', 'desc')->take(3)->get() as $post)
+                                  <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                      <div class="case-item">
+                                          <div class="case-item__thumb">
+                                              <img src="{{$post->featured}}" alt="our case">
+                                          </div>
+                                          <h6 class="case-item__title text-center"><a href="#">{{$post->title}}</a></h6>
+                                      </div>
+                                  </div>
+                                @endforeach
+                              </div>
+                          </div>
+                      </div>
                     </div>
-                    <div class="row">
-                        <div class="case-item-wrap">
-                          @foreach($laravel->posts()->orderBy('created_at', 'desc')->take(3)->get() as $post)
-                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                                <div class="case-item">
-                                    <div class="case-item__thumb">
-                                        <img src="{{$post->featured}}" alt="our case">
-                                    </div>
-                                    <h6 class="case-item__title text-center"><a href="#">{{$post->title}}</a></h6>
-                                </div>
-                            </div>
-                          @endforeach
-                        </div>
-                    </div>
-                </div>
-                <div class="padded-50"></div>
-                <div class="offers">
-                    <div class="row">
-                        <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-                            <div class="heading">
-                                <h4 class="h1 heading-title">{{ $vuejs->name }}</h4>
-                                <div class="heading-line">
-                                    <span class="short-line"></span>
-                                    <span class="long-line"></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="case-item-wrap">
-                          @foreach($vuejs->posts()->orderBy('created_at', 'desc')->take(3)->get() as $post)
-                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                                <div class="case-item">
-                                    <div class="case-item__thumb">
-                                        <img src="{{$post->featured}}" alt="our case">
-                                    </div>
-                                    <h6 class="case-item__title text-center"><a href="#">{{$post->title}}</a></h6>
-                                </div>
-                            </div>
-                          @endforeach
-                        </div>
-                    </div>
-                </div>
-                <div class="padded-50"></div>
-                <div class="offers">
-                    <div class="row">
-                        <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-                            <div class="heading">
-                                <h4 class="h1 heading-title">Laravel 5.3</h4>
-                                <div class="heading-line">
-                                    <span class="short-line"></span>
-                                    <span class="long-line"></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="case-item-wrap">
-                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                                <div class="case-item">
-                                    <div class="case-item__thumb">
-                                        <img src="{{asset('app/img/5.jpg')}}" alt="our case">
-                                    </div>
-                                    <h6 class="case-item__title"><a href="#">Investigationes demonstraverunt legere</a></h6>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4  col-md-4 col-sm-6 col-xs-12">
-                                <div class="case-item">
-                                    <div class="case-item__thumb">
-                                        <img src="{{asset('app/img/2.png')}}" alt="our case">
-                                    </div>
-                                    <h6 class="case-item__title">Claritas est etiam processus dynamicus</h6>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4  col-md-4 col-sm-6 col-xs-12">
-                                <div class="case-item">
-                                    <div class="case-item__thumb mouseover poster-3d lightbox shadow animation-disabled" data-offset="5">
-                                        <img src="{{asset('app/img/6.jpg')}}" alt="our case">
-                                    </div>
-                                    <h6 class="case-item__title">quod mazim placerat facer possim assum</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                  @endif
+                @endforeach
                 <div class="padded-50"></div>
             </div>
           </div>
